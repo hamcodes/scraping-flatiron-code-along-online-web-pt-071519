93e0bde33d4a01596e938f49c9266ed3571b662d
Jeremy Hamilton <jeremyhamilton035@gmail.com>
2019-09-12 01:33:32 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -5,9 +5,9 @@ require_relative './course.rb'
  
 class Scraper
  
-  # def get_page
-  #   Nokogiri::HTML(open("http://learn-co-curriculum.github.io/site-for-scraping/courses"))
-  # end
+  def get_page
+    Nokogiri::HTML(open("http://learn-co-curriculum.github.io/site-for-scraping/courses"))
+  end
  
   # def get_courses
   #   self.get_page.css(".post")
